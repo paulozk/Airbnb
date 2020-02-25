586024aca6a8ba5a54307d1cbea07d0fc2c7e04d
paulozk <p.m.ozkohen@student.rug.nl>
2020-02-25 14:54:52 +0100

    Simple flask-api works, hackiness fixed!

--- cleaning_api/app/routes.py
+++ cleaning_api/app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request
 from app import app
-# from cleaning.data_cleaning import clean_dataset_json
+from cleaning.data_cleaning import clean_dataset_json
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
@@ -9,13 +9,12 @@ def index():
 
 @app.route('/v1/methods/cleaning', methods=['GET'])
 def clean_dataset():
-    from cleaning.data_cleaning import clean_dataset_json
     if(request.method == 'GET'):
         # get json-ified pandas dataset that needs cleaning
         content = request.get_json()
         # send json file to cleaning package for cleaning and return json-ified clean dataset
-        clean_dataset_json = clean_dataset_json(content)
+        cleaned_dataset_json = clean_dataset_json(content)
 
         # return json-ified clean dataset to user
-        return clean_dataset_json
+        return cleaned_dataset_json
 

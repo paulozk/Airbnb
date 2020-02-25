9d851138a8e67cb1542eda583ba530018500d777
paulozk <p.m.ozkohen@student.rug.nl>
2020-02-25 13:19:21 +0100

    Initial flask cleaning api written; doesn't work completely yet

---
+++ cleaning_api/app/routes.py
@@ -0,0 +1,20 @@
+from flask import render_template, request
+from app import app
+from Airbnb.cleaning.data_cleaning import clean_dataset_json
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/v1/methods/cleaning', methods=['GET', 'POST'])
+def clean_dataset():
+    if request.method == 'GET':
+        # get json-ified pandas dataset that needs cleaning
+        content = request.get_json()
+        # send json file to cleaning package for cleaning and return json-ified clean dataset
+        clean_dataset_json = clean_dataset_json(content)
+
+        # return json-ified clean dataset to user
+        return clean_dataset_json
+
---
+++ cleaning_api/send_json.py
@@ -0,0 +1,14 @@
+# this script sends a JSON-ified pandas dataframe to the flask api
+import pandas as pd
+import requests
+
+# read the data
+data = pd.read_csv('../data/listings.csv')
+# jsonify the data
+data_json = data.to_json()
+# send json file to flask api
+response = requests.get("http://localhost:5000/v1/methods/cleaning", json=data_json)
+# response should be a json-ified version of the cleaned dataset
+# convert back to pandas dataframe, then store to disk
+data_cleaned = pd.read_json(response)
+data_cleaned.to_csv("../data/listings_cleaned.csv")
